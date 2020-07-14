6f509756e17024bbeff9e17e74fd08c84f75c360
onrtsdmr <onurtaademir@gmail.com>
2020-07-14 16:58:58 +0300

    first commit

---
+++ ServerApp/Controllers/ProductsController.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using ServerApp.Data;
+using ServerApp.Models;
+
+namespace ServerApp.Controllers {
+    // localhost:5001/api/products
+    [ApiController]
+    [Route ("api/[controller]")]
+    public class ProductsController : ControllerBase {
+
+        private readonly SocialContext _context;
+
+        public ProductsController (SocialContext context) {
+            this._context = context;
+        }
+
+        // localhost:5001/api/products
+        [HttpGet]
+        public async Task<ActionResult> GetProducts () {
+            try {
+                var products = await _context.Products.ToListAsync ();
+                return Ok (products);
+            } catch (System.Exception) {
+
+                return BadRequest ();
+            }
+        }
+        // localhost:5001/api/products/{id}
+        [HttpGet ("{id}")]
+        public async Task<IActionResult> GetProductById (int id) {
+            var p = await _context.Products.FirstOrDefaultAsync(i => i.ProductId == id);
+
+            if (p == null) {
+                return NotFound ();
+            }
+
+            return Ok (p);
+        }
+
+        // localhost:5001/api/products
+        [HttpPost]
+        public async Task<IActionResult> CreateProductAsync (Product entity) {
+            try {
+                await _context.Products.AddAsync(entity);
+                await _context.SaveChangesAsync();
+                foreach (var item in _context.Products) {
+                    System.Console.WriteLine (item.Name);
+                }
+                return CreatedAtAction(nameof(GetProductById),new {id = entity.ProductId}, entity);
+            } catch (System.Exception) {
+                return BadRequest (entity);
+            }
+        }
+        
+        //localhost/api/products/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct(int id, Product entitiy){
+            if(id!=entitiy.ProductId){
+                return BadRequest();
+            }
+            
+            var product = await _context.Products.FindAsync(id);
+
+            if(product == null) return NotFound();
+
+            product.Name = entitiy.Name;
+            product.Price = entitiy.Price;
+            product.IsApproved = entitiy.IsApproved;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (System.Exception)
+            {
+                
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        // localhost:5001/api/products/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id){
+            var product = await _context.Products.FindAsync(id);
+
+            if(product == null) return BadRequest();
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // You can also add the id parameter in the body section.
+        // localhost:5001/api/products
+        // [HttpDelete()]
+        // public async Task<IActionResult> DeleteProduct(int id){
+        //     var product = await _context.Products.FindAsync(id);
+
+        //     if(product == null) return BadRequest();
+
+        //     _context.Products.Remove(product);
+        //     await _context.SaveChangesAsync();
+
+        //     return NoContent();
+        // }
+    }
+}
