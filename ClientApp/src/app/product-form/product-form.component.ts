94fc9295437d005d8aa0f2f31f164250c0e2cfd8
onrtsdmr <onurtaademir@gmail.com>
2020-07-16 02:35:38 +0300

    product-form added.

---
+++ ClientApp/src/app/product-form/product-form.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'product-form',
+  templateUrl: './product-form.component.html',
+  styleUrls: ['./product-form.component.css']
+})
+export class ProductFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+  addProduct(name: string, price: number, isActive: boolean){
+    console.log(name);
+    console.log(price);
+    console.log(isActive);
+  }
+
+}
