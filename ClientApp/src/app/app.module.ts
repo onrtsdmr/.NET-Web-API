6ac79ae3db3a71ee95e9a270bcb5a24003af846a
onrtsdmr <onurtaademir@gmail.com>
2020-07-23 01:25:08 +0300

    Model files have been added to client side.

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -15,6 +15,11 @@ import { HomeComponent } from './components/home/home.component';
 import { MessagesComponent } from './components/messages/messages.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { ErrorInterceptor } from './services/error.interceptor';
+import { JwtModule } from '@auth0/angular-jwt';
+
+export function tokenGetter() {
+  return localStorage.getItem('token');
+}
 
 @NgModule({
   declarations: [
@@ -30,7 +35,19 @@ import { ErrorInterceptor } from './services/error.interceptor';
     MessagesComponent,
     NotfoundComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        allowedDomains: ["localhost:5000"],
+        disallowedRoutes: ['localhost:5000/api/auth'],
+      },
+    }),
+  ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
---
+++ ClientApp/src/app/models/image.ts
@@ -0,0 +1,7 @@
+export class Image{
+    id: number;
+    name: string;
+    description: string;
+    dateAdded: Date;
+    isProfile: boolean;
+}
